d3e179b2da3d8d47085851cd7a594e3b5f925f47
yshysh <yinshanhui_work@163.com>
2017-12-12 13:31:55 +0800

    generate doc

--- doc/generate_doc.sh
+++ doc/generate_doc.sh
@@ -1,7 +1,19 @@
 # build doc
 export WORK_ROOT=$(cd `dirname "$0"`;pwd)
-export BIGFLOW_PYTHON_HOME=${BIGFLOW_PYTHON_HOME:-$WORK_ROOT/../bigflow_python/python/}
+export BIGFLOW_PYTHON_HOME=$WORK_ROOT/../bigflow_python/python/
 BIGFLOW=$BIGFLOW_PYTHON_HOME/bigflow/bin/bigflow
+
+wget https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz -O protobuf-2.5.0.tar.gz && tar zxvf protobuf-2.5.0.tar.gz
+cd protobuf-2.5.0
+./autogen.sh && CXXFLAGS=-fPIC ./configure && make -j 12 && make install
+cd -
+sh $WORK_ROOT/../bigflow_python/gen_proto.sh
+
+VERSION="1.0.0.0"
+DATE=`date '+%Y_%m_%d_%H_%M_%S'`
+BIGFLOW_VERSION=${VERSION}_$DATE
+echo "bigflow_version = \"${BIGFLOW_VERSION}\"" > ${WORK_ROOT}/../bigflow_python/python/bigflow/version.py
+
 $BIGFLOW pip install sphinx
 $BIGFLOW make html
 touch _build/html/.touch _build/html/.nojekyll
