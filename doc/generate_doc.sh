fff224adc3c6ccbddbf5565ad82f1423db95be4a
yshysh <yinshanhui_work@163.com>
2017-12-23 08:17:59 +0800

    test

--- doc/generate_doc.sh
+++ doc/generate_doc.sh
@@ -23,7 +23,7 @@ cd -
 
 rm -rf html && mkdir html
 cd html
-touch _build/html/.touch _build/html/.nojekyll
+touch .touch .nojekyll
 cd -
 mv ../zh/_build/html zh
 #git clone -b en_doc https://github.com/yshysh/bigflow.git
