85a320e828af67c47160965e7ba5c812e9b60db0
Travis CI <travis@travis-ci.org>
2018-01-10 14:37:58 +0800

    test

--- scripts/build_doc_po_file.sh
+++ scripts/build_doc_po_file.sh
@@ -14,7 +14,7 @@ copy_file() {
 }
 
 git_add() {
-    git checkout master
+    git checkout -b doc
     git add doc/locales/en/LC_MESSAGES/*.po
     git add doc/locales/zh/LC_MESSAGES/*.po
     git commit -m "Update po files: $TRAVIS_BUILD_NUMBER"
