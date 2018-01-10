fbeb906c0c6e99a830302881f66121fc410a3fa4
Travis CI <travis@travis-ci.org>
2018-01-10 14:46:47 +0800

    test

--- scripts/build_doc_po_file.sh
+++ scripts/build_doc_po_file.sh
@@ -14,7 +14,7 @@ copy_file() {
 }
 
 git_add() {
-    git checkout -b doc
+    git checkout -b doc_test
     git add doc/locales/en/LC_MESSAGES/*.po
     git add doc/locales/zh/LC_MESSAGES/*.po
     git commit -m "Update po files: $TRAVIS_BUILD_NUMBER"
@@ -22,7 +22,7 @@ git_add() {
 
 git_push() {
     git remote add origin-doc https://$GITHUB_TOKEN@github.com/yshysh/bigflow.git
-    git push --set-upstream origin-doc doc
+    git push --set-upstream origin-doc doc_test
 }
 
 git_setup
